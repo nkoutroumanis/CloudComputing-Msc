64362b916358037fca8782ef926cf9d7bf5a560d
Panagiotis Nikitopoulos <nikpanos@users.noreply.github.com>
2019-10-24 12:20:11 +0300

    Update HotelsMapper.java
    Switched from using String.split to the more efficient manual String parsing.

--- src/main/java/com/github/nkoutroumanis/HotelsMapper.java
+++ src/main/java/com/github/nkoutroumanis/HotelsMapper.java
@@ -24,24 +24,52 @@ import org.apache.hadoop.mapred.Reporter;
 public class HotelsMapper extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, HotelsRestaurantsWritable> {
 
     private static Grid grid;
+    private int prevPos = 0;
+    private int nextPos = 0;
     
     @Override
     public void configure(JobConf job) {
         grid = Grid.newInstance(job.getFloat("radius", 0), job.getInt("xCellsDivision", 1), job.getInt("yCellsDivision", 1));
-    }    
+    }
+    
+    private String getNextToken(String str) {
+        if (nextPos == str.length())
+            return null;
+        if (nextPos != 0) {
+            prevPos = nextPos + 2;
+        }
+        nextPos = str.indexOf("\\|", prevPos);
+        if (nextPos < 0)
+            nextPos = str.length();
+        return str.substring(prevPos, nextPos);
+    }
+
+    private String getNextToken(String str, int skip) {
+        if (skip < 0) {
+            return null;
+        }
+        int count = 0;
+        String res;
+        do {
+            res = getNextToken(str);
+        }
+        while ((count++ < skip) && (res != null));
+        return res;
+    }
+    
     @Override
     public void map(LongWritable key, Text value, OutputCollector<IntWritable, HotelsRestaurantsWritable> oc, Reporter rprtr) throws IOException {
         String s = value.toString();
-        String[] tokens = s.split("\\|");
 
-        float[] coordinates = new float[2];
-        coordinates[0] = Float.parseFloat(tokens[4]);//lat
-        coordinates[1] = Float.parseFloat(tokens[5]);//lon
+        String name = getNextToken(str);
+        String address = getNextToken(str);
+        
+        float[] coordinates = new float[2](Float.parseFloat(getNextToken(str, 2), getNextToken(str));
         
         if(grid.contains(coordinates[0], coordinates[1]))
         {
             IntWritable iw = new IntWritable(grid.putHotelsInCells(coordinates[0], coordinates[1]));
-            HotelsRestaurantsWritable hrw = new HotelsRestaurantsWritable(true, tokens[0]+" - "+tokens[1], coordinates[0], coordinates[1], "");
+            HotelsRestaurantsWritable hrw = new HotelsRestaurantsWritable(true, name+" - "+address, coordinates[0], coordinates[1], "");
             
             oc.collect(iw, hrw);
             
